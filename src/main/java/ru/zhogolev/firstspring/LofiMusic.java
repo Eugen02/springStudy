949e8d3aa951f12e7807368ad07a3862079d3bf2
Eugen02 <zhenyazhogolev70@gmail.com>
2020-12-06 15:40:10 +0200

    Spring alish l:6

---
+++ src/main/java/ru/zhogolev/firstspring/LofiMusic.java
@@ -0,0 +1,8 @@
+package ru.zhogolev.firstspring;
+
+public class LofiMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Lofi repsodi";
+    }
+}
--- src/main/java/ru/zhogolev/firstspring/MusicPlayer.java
+++ src/main/java/ru/zhogolev/firstspring/MusicPlayer.java
@@ -1,19 +1,26 @@
 package ru.zhogolev.firstspring;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+     List<Music> musicList = new ArrayList<>();
     private String name;
     private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(Music musicList, Music musicList1, Music musicList2) {
+        this.musicList.add(musicList);
+        this.musicList.add(musicList1);
+        this.musicList.add(musicList2);
     }
 
     public MusicPlayer() {}
 
-    public void setMusic(Music music) {
-        this.music = music;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void setName(String name) {
@@ -25,15 +32,52 @@ public class MusicPlayer {
     }
 
     public void playMusic(){
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: musicList
+             ) {
+            System.out.println("Music Play = " + music.getSong());
+        }
     }
 
     @Override
     public String toString() {
         return "MusicPlayer{" +
-                "music=" + music +
                 ", name='" + name + '\'' +
                 ", volume=" + volume +
                 '}';
-    }
+   }
+//    private Music music;
+//    private String name;
+//    private int volume;
+//
+//    //IoC
+//    public MusicPlayer(Music music) {
+//        this.music = music;
+//    }
+//
+//    public MusicPlayer() {}
+//
+//    public void setMusic(Music music) {
+//        this.music = music;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setVolume(int volume) {
+//        this.volume = volume;
+//    }
+//
+//    public void playMusic(){
+//        System.out.println("Playing: " + music.getSong());
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MusicPlayer{" +
+//                "music=" + music +
+//                ", name='" + name + '\'' +
+//                ", volume=" + volume +
+//                '}';
+//    }
 }
