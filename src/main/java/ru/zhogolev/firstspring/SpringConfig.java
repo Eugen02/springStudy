f2d60880497e8596fd9d23fe51fe9a491f0dc3ac
Eugen02 <zhenyazhogolev70@gmail.com>
2020-12-08 19:38:03 +0200

    spring annot l:13, and task for self study, with SpringConfig

---
+++ src/main/java/ru/zhogolev/firstspring/SpringConfig.java
@@ -0,0 +1,44 @@
+package ru.zhogolev.firstspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+
+    @Bean
+    public ClassicalMusic classicalMusic(){
+        return new ClassicalMusic();
+    }
+    @Bean
+    public RockMusic rockMusic(){
+        return new RockMusic();
+    }
+    @Bean
+    public LofiMusic lofiMusic(){
+        return new LofiMusic();
+    }
+
+    @Bean
+    public List<Music> musicList(){
+        return Arrays.asList(classicalMusic(),rockMusic(),lofiMusic());
+    }
+
+    @Bean
+    public MusicPlayer musicPlayer(){
+        return new MusicPlayer(musicList());
+    }
+
+    @Bean
+    public Computer computer(){
+        return new Computer(musicPlayer());
+    }
+
+
+}
